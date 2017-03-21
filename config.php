6f1495206685c34f2b9ce05695e11b21e7c3fde6
jpatinop <jpatinop@gmail.com>
2017-03-20 21:28:15 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  App-specific configuration for your game.
+
+  You will need to configure:
+    - the URL to the server where you are hosting the game source code.
+    - create a new App and retrieve the app ID and namespace from Facebook
+    Developer site at https://developers.facebook.com/apps
+*/
+  $serverUrl = "https://www.curioerp.com/gameslyce/plugins/fbpro";
+  $appNamespace = "gametestfeatures";
+  $appId = "864246060388493";
+?>
